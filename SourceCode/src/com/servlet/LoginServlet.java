5dfa7fd2d2a004163329f6fa85a7b6a00dc10d7e
ouyangkai <ouyangkai@bytedance.com>
2019-06-17 21:15:18 +0800

    feat(ouykai): 增加数据库连接逻辑
    Change-Id: I30bf9f6b6e3297f7ec1281fa28f0e9f992256877

--- SourceCode/src/com/servlet/LoginServlet.java
+++ SourceCode/src/com/servlet/LoginServlet.java
@@ -1,4 +1,5 @@
 package com.servlet;
+
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
@@ -15,42 +16,44 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.websocket.Session;
 
+public class LoginServlet extends HttpServlet {
 
-public class LoginServlet extends HttpServlet{
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
 
-	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {  
-        doGet(request, response);  
-    }  
-	protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
-		String wname=request.getParameter("wname");
-		String password=request.getParameter("password");
+		String wname = request.getParameter("wname");
+		String password = request.getParameter("password");
 		request.setAttribute("wname", wname);
-		
-		String driverName="com.microsoft.sqlserver.jdbc.SQLServerDriver";
-		String dbURL="jdbc:sqlserver://localhost:1433;DatabaseName=WorkExtraDB";
-		String userName="sa";
-		String userPwd="guyuchao";
-		Connection dbConn=null;
+
+		String driverName = "com.mysql.jdbc.Driver";
+		String dbURL = "jdbc:mysql://172.18.187.10:3306/boke16337157";
+		String userName = "user";
+		String userPwd = "123";
+		Connection dbConn = null;
+
 		try {
-			dbConn= DriverManager.getConnection(dbURL,userName,userPwd);
-			String sql="select * from worker where wname= ? and password= ?";
-			PreparedStatement ps=dbConn.prepareStatement(sql);
+			dbConn = DriverManager.getConnection(dbURL, userName, userPwd);
+			String sql = "select * from worker where wname= ? and password= ?";
+			PreparedStatement ps = dbConn.prepareStatement(sql);
 			ps.setString(1, wname);
-			ps.setString(2,password);
-			ResultSet rs=ps.executeQuery();
-			if(rs.next()){
-				HttpSession session=request.getSession();
+			ps.setString(2, password);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				HttpSession session = request.getSession();
+				System.out.print(session);
 				session.setAttribute("wno", rs.getInt("wno"));
-				if(rs.getString("pwno") != null){
+				if (rs.getString("pwno") != null) {
 					request.getRequestDispatcher("welcome_worker.jsp").forward(request, response);
-				}
-				else{
-					
+				} else {
+
 					request.getRequestDispatcher("welcome_admin.jsp").forward(request, response);
 				}
-			}
-			else{
+			} else {
 				request.getRequestDispatcher("error.jsp").forward(request, response);
 			}
 			ps.close();
@@ -60,5 +63,5 @@ public class LoginServlet extends HttpServlet{
 			e.printStackTrace();
 		}
 	}
-	
+
 }
